84fd3197a648bf4eb21ac9770d919ea47d2ec793
ReemWSW <widsawa.d31@gmail.com>
2025-07-08 07:03:38 +0700

    refactor: Simplify UserInfoSection constructor by using 'super.key'

--- lib/widgets/user_info_section.dart
+++ lib/widgets/user_info_section.dart
@@ -9,10 +9,10 @@ class UserInfoSection extends StatelessWidget {
   final BluetoothController btController;
 
   const UserInfoSection({
-    Key? key,
+    super.key,
     required this.user,
     required this.btController,
-  }) : super(key: key);
+  });
 
   @override
   Widget build(BuildContext context) {
